9c20928224bdf81465f6fccf59fd2fe756e3f2d6
Dmitry Kologrivov <dkologrivov@scnsoft.com>
2017-11-17 12:49:25 +0300

    Remove extra headers

--- message.go
+++ message.go
@@ -2,10 +2,7 @@ package wsqueue
 
 import (
 	"encoding/json"
-	"os"
-	"reflect"
 	"strconv"
-	"time"
 
 	"github.com/satori/go.uuid"
 )
@@ -26,21 +23,12 @@ func newMessage(data interface{}) (*Message, error) {
 
 	switch data.(type) {
 	case string, *string:
-		m.Header["content-type"] = "string"
 		m.Body = data.(string)
 	case int, *int, int32, *int32, int64, *int64:
-		m.Header["content-type"] = "int"
 		m.Body = strconv.Itoa(data.(int))
 	case bool, *bool:
-		m.Header["content-type"] = "bool"
 		m.Body = strconv.FormatBool(data.(bool))
 	default:
-		m.Header["content-type"] = "application/json"
-		if reflect.TypeOf(data).Kind() == reflect.Ptr {
-			m.Header["application-type"] = reflect.ValueOf(data).Elem().Type().String()
-		} else {
-			m.Header["application-type"] = reflect.ValueOf(data).Type().String()
-		}
 		b, err := json.Marshal(data)
 		if err != nil {
 			return nil, err
@@ -48,8 +36,6 @@ func newMessage(data interface{}) (*Message, error) {
 		m.Body = string(b)
 	}
 	m.Header["id"] = uuid.NewV1().String()
-	m.Header["date"] = time.Now().String()
-	m.Header["host"], _ = os.Hostname()
 	return &m, nil
 }
 
@@ -67,14 +53,4 @@ func (m *Message) String() string {
 //ID returns message if
 func (m *Message) ID() string {
 	return m.Header["id"]
-}
-
-//ContentType returns content-type
-func (m *Message) ContentType() string {
-	return m.Header["content-type"]
-}
-
-//ApplicationType returns application-type. Empty if content-type is not application/json
-func (m *Message) ApplicationType() string {
-	return m.Header["application-type"]
 }
